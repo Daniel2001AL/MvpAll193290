ef5ceefde79744c2c2b67302ee93bcc318e5eba9
DanielRios <193290@ids.upchiapas.edu.mx>
2022-06-17 11:55:50 -0500

    final changes c2 activity

--- lib/homeview/widget/information_person_widget.dart
+++ lib/homeview/widget/information_person_widget.dart
@@ -15,84 +15,73 @@ class InfomationWidget extends StatelessWidget {
       height: size.height * 0.2,
       child: Column(
         children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  left: size.width * 0.05,
-                  top: size.height * 0.0001,
-                  bottom: size.height * 0.025,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(top: size.height * 0.01),
-                  child: RichText(
-                    textAlign: TextAlign.start,
-                    text: TextSpan(
-                      children: <InlineSpan>[
-                        const TextSpan(
-                          text: 'Hola',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 22,
-                          ),
-                        ),
-                        TextSpan(
-                          text: ' Juan',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green.shade400,
-                            fontSize: 22,
-                          ),
-                        ),
-                        const TextSpan(
-                          text: ',',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 30,
-                          ),
-                        ),
-                      ],
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: RichText(
+              textAlign: TextAlign.start,
+              text: TextSpan(
+                children: <InlineSpan>[
+                  const TextSpan(
+                    text: 'Hola',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 22,
                     ),
                   ),
-                ),
+                  TextSpan(
+                    text: ' Juan',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green.shade400,
+                      fontSize: 22,
+                    ),
+                  ),
+                  const TextSpan(
+                    text: ',',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-          Container(
-            // color: Colors.blue,
-            height: size.height * 0.05,
+          SizedBox(
+            //color: Colors.blue,
+            height: size.height * 0.09,
             child: Row(
               children: [
-                Container(
-                  // color: Colors.red,
-                  padding: const EdgeInsets.only(left: 18, right: 0),
+                SizedBox(
+                  //color: Colors.red,
                   width: size.width * 0.15,
-                  height: size.height * 0.2,
+                  height: size.height * 0.20,
                   child: Image.asset("assets/onBoarding/B5.png"),
                 ),
-                Container(
-                  // color: Colors.amber,
-                  width: size.width * 0.3,
-                  height: size.height * 0.05,
+                SizedBox(
+                  //color: Colors.amber,
+                  width: size.width * 0.35,
+                  height: size.height * 0.09,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "Entregar ahora",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      //Padding(padding: EdgeInsets.only(bottom: 0)),
-                      ExpansionTile(
-                        title: Padding(
-                          padding: EdgeInsets.all(0),
-                          child: Text(
-                            "Calle 10 9",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 10),
-                          ),
+                    children: [
+                      Container(
+                        width: size.width * 0.35,
+                        height: size.height * 0.02,
+                        margin: const EdgeInsets.only(left: 15),
+                        child: const Text(
+                          "Entregar ahora",
+                          style: TextStyle(color: Colors.grey),
                         ),
-                      )
+                      ),
+                      const ExpansionTile(
+                        title: Text(
+                          "Calle 10 9",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 10),
+                        ),
+                      ),
                     ],
                   ),
                 )
